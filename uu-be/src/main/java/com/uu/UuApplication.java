a10f369311f36a0a0c3283ec7545573e15bab12e
subabe <983523649@qq.com>
2026-03-22 16:53:40 +0800

    feat: initialize Spring Boot project with Gradle

---
+++ uu-be/src/main/java/com/uu/UuApplication.java
@@ -0,0 +1,11 @@
+package com.uu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UuApplication.class, args);
+    }
+}
